2f38c0892e9ae041b3734b57c053793a7c0f72a8
mariananetto <gabriela.19288427@aluno.pensi.com.br>
2026-04-04 16:54:27 -0300

    gg!!!!!!!!!!!!!!!!!!!!!!!!!!

--- ai-model.js
+++ ai-model.js
@@ -1004,7 +1004,7 @@ class MeteorGuardAI {
             riskScore,
             percentage,
             level,
-            title: title + confText,
+            title: title,
             icon,
             color,
             analysis,
